93884738e1e7e46d2f046ea1035de9993fd29bc5
diablo2000es <diablo2000es@hotmail.com>
2015-06-13 00:09:31 +0200

    primera pregunta

---
+++ controllers/quiz_controllers.js
@@ -0,0 +1,19 @@
+// get /quizes/question
+
+exports.question = function(req,res){
+    //console.log("render");
+    res.render("quizes/question", {pregunta: "Capital de Italia"});
+}
+
+
+
+
+
+exports.answer = function (req, res){
+    if (req.query.respuesta === "Roma"){
+        res.render("quizes/answers", {respuesta: "Correcto"});
+        
+    } else {
+        res.render("quizes/answers", {respuesta: "Incorrecto"});
+    }
+};
