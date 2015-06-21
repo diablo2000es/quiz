be438aea8d493b624cd4c90aeb81ce0478cf0be5
diablo2000es <diablo2000es@hotmail.com>
2015-06-22 00:58:35 +0200

    Lista de preguntas

--- controllers/quiz_controllers.js
+++ controllers/quiz_controllers.js
@@ -1,31 +1,43 @@
 var models = require("../models/models.js");
 
-// get /quizes/question
+// get /quizes/:id
 
-exports.question = function(req,res){
+exports.show = function(req,res){
     //console.log("render");
     //res.render("quizes/question", {pregunta: "Capital de Italia"});
-    models.Quiz.findAll().success(function(quiz){
-       res.render("quizes/question", {pregunta: quiz[0].pregunta})
+    //models.Quiz.findAll().success(function(quiz){
+    //   res.render("quizes/question", {pregunta: quiz[0].pregunta})
+    //})
+    models.Quiz.find(req.params.quizId).then(function(quiz){
+        console.log(quiz);
+        res.render("quizes/show", {quiz:quiz});
     })
 
-}
+};
 
 
 
 
-
+//fet /quizes/:id/aswer
 exports.answer = function (req, res){
-    models.Quiz.findAll().success(function(quiz){
-        if (req.query.respuesta === quiz[0].respuesta){
-        res.render("quizes/answers", {respuesta: "Correcto"});
+    models.Quiz.find(req.params.quizId).then(function(quiz){
+        if (req.query.respuesta === quiz.respuesta){
+        res.render("quizes/answers",
+                   {quiz:quiz, respuesta: "Correcto"});
         
     } else {
-        res.render("quizes/answers", {respuesta: "Incorrecto"});
+        res.render("quizes/answers",
+                   {quiz:quiz, respuesta: "Incorrecto"});
     }
 
     })
 
+};
+
+exports.index = function(req,res) {
+    models.Quiz.findAll().then(function(quizes){
+        res.render("quizes/index.ejs", {quizes:quizes});
+    })
 };
 
 exports.author = function (req, res){
