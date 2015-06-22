0b31547f006294ec7cb433e2150c68473d51f705
diablo2000es <diablo2000es@hotmail.com>
2015-06-22 23:41:06 +0200

    Autoload

--- controllers/quiz_controllers.js
+++ controllers/quiz_controllers.js
@@ -1,5 +1,20 @@
 var models = require("../models/models.js");
 
+//Autoload - factoriza el codigo su la ruta incluye :quizId
+exports.load = function(req, res, next, quizId){
+    models.Quiz.find(quizId).then(
+        function(quiz){
+            if (quiz){
+                req.quiz = quiz;
+                next();
+            } else {next(new Error("No existe quizId="+quizId));}
+        }
+    ).catch(function(error){next(error);});
+}
+
+
+
+
 // get /quizes/:id
 
 exports.show = function(req,res){
@@ -8,36 +23,33 @@ exports.show = function(req,res){
     //models.Quiz.findAll().success(function(quiz){
     //   res.render("quizes/question", {pregunta: quiz[0].pregunta})
     //})
-    models.Quiz.find(req.params.quizId).then(function(quiz){
-        console.log(quiz);
-        res.render("quizes/show", {quiz:quiz});
-    })
+    //models.Quiz.find(req.params.quizId).then(function(quiz){
+        //console.log(quiz);
+        res.render("quizes/show", {quiz: req.quiz});
+    };
 
-};
+
 
 
 
 
 //fet /quizes/:id/aswer
 exports.answer = function (req, res){
-    models.Quiz.find(req.params.quizId).then(function(quiz){
-        if (req.query.respuesta === quiz.respuesta){
-        res.render("quizes/answers",
-                   {quiz:quiz, respuesta: "Correcto"});
-        
-    } else {
-        res.render("quizes/answers",
-                   {quiz:quiz, respuesta: "Incorrecto"});
-    }
+    var resultado = "Incorrecto";
+    //models.Quiz.find(req.params.quizId).then(function(quiz){
+        if (req.query.respuesta === req.quiz.respuesta){
+            resultado = "Correcto";
+        }
+        res.render("quizes/answer", {quiz: req.quiz, respuesta: resultado});
 
-    })
+    };
 
-};
+
 
 exports.index = function(req,res) {
     models.Quiz.findAll().then(function(quizes){
         res.render("quizes/index.ejs", {quizes:quizes});
-    })
+    }).catch(function(error){next(error);})
 };
 
 exports.author = function (req, res){
